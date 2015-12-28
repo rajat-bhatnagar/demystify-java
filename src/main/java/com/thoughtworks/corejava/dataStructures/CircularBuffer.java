02376cc2941559fbfb0a7fbe72909c338a1d40c3
rajatBhatnagar <rajat_bhatnagar@outlook.com>
2015-12-28 13:13:11 -0500

    Added Commons collection dependency in pom.xml and implementation of CircularBuffer using CircularFifoBuffer

---
+++ src/main/java/com/thoughtworks/corejava/dataStructures/CircularBuffer.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.corejava.dataStructures;
+
+import org.apache.commons.collections.buffer.CircularFifoBuffer;
+/**
+ * Created by rajatbhatnagar on 12/28/15.
+ */
+public class CircularBuffer {
+    //Circular buffer is a linear data structure which follows the FIFO principle
+    public static void main(String[] args) {
+        CircularFifoBuffer buf = new CircularFifoBuffer(4);
+        //We use CircularFifoBuffer from apache commons collection for the implementation
+        buf.add("A");
+        buf.add("B");
+        buf.add("C");
+        buf.add("D");
+        buf.add("E");
+        buf.remove();
+        System.out.println("buf = " + buf);
+    }
+}
