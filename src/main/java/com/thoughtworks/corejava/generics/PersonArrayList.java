46c7b51f91613eb6fb651dd3519eb367b5be1def
rajatBhatnagar <rajat_bhatnagar@outlook.com>
2015-12-29 07:42:04 -0500

    Added classes to demonstrate the usage of ArrayList with Generics

---
+++ src/main/java/com/thoughtworks/corejava/generics/PersonArrayList.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.corejava.generics;
+
+import com.thoughtworks.corejava.arrays.Person;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by rajatbhatnagar on 12/29/15.
+ */
+public class PersonArrayList {
+    public static void main(String[] args) {
+        Person Swenson = new Person(40,"Mike");
+        Person Gagan = new Person(45,"Gagan");
+        Person Bunny = new Person(40,"Bunny");
+        List<Person> persons = new ArrayList<Person>();
+        persons.add(Swenson);
+        persons.add(Gagan);
+        persons.add(Bunny);
+        System.out.println("persons = " + persons);
+        //OPTION 1 for looping over
+        System.out.println("\nOPTION 1 for looping over :: Use ITERATOR");
+        Iterator<Person> iterator = persons.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        //OPTION 2 for looping over
+        System.out.println("\nOPTION 2 for looping over :: USE FOR EACH");
+        for(Person person : persons){
+            System.out.println("person = " + person);
+        }
+    }
+}
