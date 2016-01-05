af90c2d725edfbdd4641ae3d614086ad6cb0aa1f
rajatBhatnagar <rajat_bhatnagar@outlook.com>
2016-01-05 03:14:00 -0500

    Code clean-up

--- src/main/java/com/thoughtworks/corejava/generics/GenericVarArgsDemo.java
+++ src/main/java/com/thoughtworks/corejava/generics/GenericVarArgsDemo.java
@@ -8,9 +8,11 @@ import java.util.List;
  */
 public class GenericVarArgsDemo {
     public static void main(String[] args) {
-    List<Integer> ints = toList(new Integer[]{ 1,2,3,4});
+        //Explicit parameters as below are not required but are useful
+        //when the generic method has more number of arguments of different types
+    List<Integer> ints =  GenericVarArgsDemo.<Integer>toList(1,2,3,4);
     addAll(ints,5,6,7,8);
-    List<String> strings = toList(new String[]{"Believe","In","Yourself"});
+    List<String> strings = GenericVarArgsDemo.<String>toList("Believe","In","Yourself");
     addAll(strings,"Rajat");
     }
     //A generic var args method to takes in a list and an array of any number of arguments ,
@@ -22,7 +24,7 @@ public class GenericVarArgsDemo {
         System.out.println("Elements in list are : "+list);
     }
 
-    public static <T> List<T> toList(T[] arr){
+    public static <T> List<T> toList(T... arr){
         List<T> list = new ArrayList<>();
         for(T element : arr){
             list.add(element);
