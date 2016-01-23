102eaa70638fc3401d850efe231751d9cb68842f
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-01-23 15:09:07 -0500

    Added class to demonstrate how to write a generic method out of a collection using Comparator

---
+++ src/main/java/com/thoughtworks/corejava/generics/FindMaxElementCollGenericMethod.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.corejava.generics;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class FindMaxElementCollGenericMethod {
+
+    //A generic method to find the maximum element out of a collection
+    public static <T extends Comparable<T>> T max(Collection<T> collection){
+        T candidate = collection.iterator().next();
+        for(T element : collection){
+            if(candidate.compareTo(element)<0){
+                candidate = element;
+            }
+        } return candidate;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum element in collection is :: "+max(Arrays.asList(12,17,19,7,765,-89)));
+        assert max(Arrays.asList(12,17,19,7,765,-89)) == 765;
+    }
+}
