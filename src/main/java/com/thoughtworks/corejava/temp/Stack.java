c61b662edbe9e50f97667c3dbfcd0b03aa6f77b0
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-01-26 00:05:23 -0500

    Added set-up class for later

---
+++ src/main/java/com/thoughtworks/corejava/temp/Stack.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.corejava.temp;
+
+/**
+ * Created by rajatbhatnagar on 1/25/16.
+ */
+public interface Stack {
+    public boolean empty();
+    public void push(Object element);
+    public Object pop();
+}
