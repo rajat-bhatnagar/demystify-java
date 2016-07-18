67e8cb3acf9ed46975b30f7dd37dc15c64c81af5
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-07-17 23:12:26 -0400

    Modified method names of hexadecimal numbers class

--- src/main/java/com/thoughtworks/corejava/numbers/CreateHexadecimalNumbers.java
+++ src/main/java/com/thoughtworks/corejava/numbers/CreateHexadecimalNumbers.java
@@ -16,24 +16,26 @@ public class CreateHexadecimalNumbers {
         return buffer.array();
     }
 
-    public static String createHexString(long sequenceNumber){
+    public static String createHexNum(long sequenceNumber){
         String hexString = Hex.encodeHexString(longToBytes(sequenceNumber));
         //Removing leading zeros
         return hexString.substring(8,16);
     }
 
-    public static Long createHexNum(String hexString){
+    public static Long decodeHexNum(String hexString){
         return Long.decode("0x"+hexString);
     }
+
+
 
     public static void main(String[] args) {
         //Checking for boundary conditions
         long LONG_MAX = 2147483647;
         System.out.println("Input Number :: " + LONG_MAX);
-        System.out.println("HexString : "+createHexString(LONG_MAX) + " :: HexNum : " +createHexNum(createHexString(LONG_MAX)));
+        System.out.println("HexString : "+createHexNum(LONG_MAX) + " :: HexNum : " +decodeHexNum(createHexNum(LONG_MAX)));
         //Checking for numbers from 0 to 15
         for(int i =0 ;i <17;i++){
-            System.out.println("HexString : "+createHexString(i) + " :: HexNum : " +createHexNum(createHexString(i)));
+            System.out.println("HexString : "+createHexNum(i) + " :: HexNum : " +decodeHexNum(createHexNum(i)));
         }
     }
 }
