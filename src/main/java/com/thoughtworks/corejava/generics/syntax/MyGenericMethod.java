10fd129cbb0504254ca0ca7da4ab173a9580ad9b
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2019-03-31 21:49:59 -0500

    31stMarch2019: Added implementation for a class generic method which returns the count of elements greater than the input element in the existing list using Comparable instead of Greater than operators

--- src/main/java/com/thoughtworks/corejava/generics/syntax/MyGenericMethod.java
+++ src/main/java/com/thoughtworks/corejava/generics/syntax/MyGenericMethod.java
@@ -7,6 +7,7 @@ public class MyGenericMethod {
                 "FRIDAY", "SATURDAY", "SUNDAY"};
         print(numbers);
         print(daysOfWeek);
+        System.out.println(" Number of elements greater than 30 : "+countGreaterThan(numbers, 30));
     }
 
     public static <E> void print(E[] list) {
@@ -14,5 +15,27 @@ public class MyGenericMethod {
             System.out.print(element + " ");
         }
         System.out.println(" ");
+    }
+
+    /*
+    ** A method which compares the incoming element to the remaining elements in the list
+    ** and returns the count of the number of elements which are greater than the elements
+    * in the list
+    *
+    * See the usage of Comparable.compareTo instead of greater than operator
+    *
+    * input = {10, 20, 30}
+    * input = 20
+    * Result = 1
+     */
+
+    private static  <T extends Comparable<T>> int countGreaterThan(T[] list, T input){
+        int count = 0;
+        for(T element : list){
+            if(element.compareTo(input) > 0){
+                count++;
+            }
+        }
+        return count;
     }
 }
